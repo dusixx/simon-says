eb1c1d9f80ee35cf7817770e20c62084fe842537
dusixx <113414935+dusixx@users.noreply.github.com>
2025-01-10 00:46:43 +0200

    feat: add helpers.js

--- simon-says/js/helpers.js
+++ simon-says/js/helpers.js
@@ -1,11 +1,13 @@
 const toStr = Object.prototype.toString;
 
 export const getTypeName = (v) => toStr.call(v).slice(8, -1);
+
 export const isStr = (v) => typeof v === 'string';
 export const isInt = (v) => Number.isInteger(Number(v));
+export const isFunc = (v) => typeof v === 'function';
 
 export const isIterable = (v) => {
-  if (typeof v?.[Symbol.iterator] === 'function') {
+  if (isFunc(v?.[Symbol.iterator])) {
     try {
       for (let _ of v) {
         return true;
@@ -13,4 +15,33 @@ export const isIterable = (v) => {
     } catch {}
   }
   return false;
+};
+
+export const fitIntoRange = (v, min, max) => {
+  const res = Math.max(min, Math.min(max, v));
+  return Number.isNaN(res) ? null : res;
+};
+
+export const rndInt = (min, max) => {
+  return Math.round(min + Math.random() * (max - min));
+};
+
+export const getRandomColor = ({ min = 0, max = 255 } = {}) => {
+  const rgb = [0, 0, 0].map((v) => rndInt(min, max));
+  const hex = rgb.map((v) => v.toString(16).padStart(2, 0));
+
+  return { rgb, hex: `#${hex.join('')}` };
+};
+
+export const applyColorAlpha = (hexColor, opacity = 1) => {
+  const alpha = fitIntoRange(opacity, 0, 1) ?? 1;
+  const alphaHex = Math.floor(alpha * 255)
+    .toString(16)
+    .padStart(2, 0);
+
+  return `${hexColor}${alphaHex}`;
+};
+
+export const sleep = async (tio) => {
+  await new Promise((resolve) => setTimeout(resolve, tio));
 };
