f191cd215453aa3b1d198d4eb63ee769a17c7528
dusixx <113414935+dusixx@users.noreply.github.com>
2025-01-12 17:20:51 +0200

    feat: add app

---
+++ simon-says/js/app.js
@@ -0,0 +1,179 @@
+import { sleep, rndInt } from './helpers.js';
+import * as refs from './refs.js';
+
+const {
+  difficulty,
+  keyboard,
+  userInput,
+  btnStart,
+  btnRepeat,
+  controls,
+  roundsCounter,
+} = refs;
+
+const caption = {
+  REPEAT: btnRepeat.text,
+  START: btnStart.text,
+  NEXT: 'Next',
+  NEW_GAME: 'New game',
+};
+
+let sequence = '';
+let attemptsLeft = 1;
+let repeatBtnMode = 'repeat'; // repeat|next
+let startBtnMode = 'start'; // start|newgame
+
+//
+// Helpers
+//
+
+const updateKeyboard = (v) => {
+  const diffLevel = {
+    easy: /[0-9]/,
+    medium: /[a-z]/i,
+    hard: /.*/,
+  };
+  const pattern = diffLevel[v.toLowerCase()];
+  if (pattern) {
+    keyboard.showKeys(pattern);
+  }
+};
+
+export const generateSequence = ({ round = 1, difficulty = 'easy' } = {}) => {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const range = {
+    easy: [0, 9],
+    medium: [10, chars.length - 1],
+    hard: [0, chars.length - 1],
+  };
+  return Array.from(
+    { length: round * 2 },
+    (v) => chars[rndInt(...range[difficulty.toLowerCase()])]
+  ).join('');
+};
+
+const init = () => {
+  attemptsLeft = 1;
+
+  btnRepeat.hide();
+  difficulty.disabled = false;
+
+  roundsCounter.value = 1;
+  roundsCounter.hide();
+
+  userInput.clear();
+  userInput.visibile = false;
+
+  keyboard.disabled = true;
+  updateKeyboard(difficulty.value);
+};
+
+const startNewRound = async () => {
+  attemptsLeft = 1;
+
+  repeatBtnMode = 'repeat';
+  btnRepeat.text = caption.REPEAT;
+  btnRepeat.disabled = false;
+  btnRepeat.show();
+
+  difficulty.disabled = true;
+  keyboard.repaint();
+  roundsCounter.show();
+
+  userInput.visibile = true;
+  userInput.clear();
+
+  await showSequence();
+};
+
+const showSequence = async ({ repeat = false, delayBefore = 700 } = {}) => {
+  btnStart.disabled = true;
+  btnRepeat.disabled = true;
+
+  sequence = repeat
+    ? sequence
+    : generateSequence({
+        round: roundsCounter.value,
+        difficulty: difficulty.value,
+      });
+
+  console.clear();
+  console.log(
+    `sequence: "${sequence}"\nlength: ${sequence.length}\nrepeated: ${repeat}`
+  );
+
+  await sleep(delayBefore);
+  await keyboard.highlight({ sequence, delay: 500, duration: 300 });
+
+  btnStart.disabled = false;
+  btnRepeat.disabled = !attemptsLeft;
+};
+
+init();
+
+//
+// Listeners
+//
+
+difficulty.onChange = (value) => {
+  updateKeyboard(value);
+};
+
+btnStart.onClick = async () => {
+  if (startBtnMode === 'start') {
+    btnStart.text = caption.NEW_GAME;
+    startBtnMode = 'newgame';
+    await startNewRound();
+  } else {
+    btnStart.text = caption.START;
+    startBtnMode = 'start';
+    init();
+  }
+};
+
+btnRepeat.onClick = async () => {
+  if (repeatBtnMode === 'repeat') {
+    userInput.clear();
+
+    await showSequence({ repeat: true });
+
+    btnRepeat.disabled = true;
+    attemptsLeft -= 1;
+  } else {
+    btnRepeat.text = caption.REPEAT;
+    repeatBtnMode = 'repeat';
+
+    roundsCounter.value += 1;
+    startNewRound();
+  }
+};
+
+keyboard.onClick = (char) => {
+  userInput.value += char;
+  const len = userInput.value.length;
+
+  // right
+  if (userInput.value === sequence) {
+    if (roundsCounter.value === roundsCounter.max) {
+      userInput.success('You won!🥳');
+      btnRepeat.disabled = true;
+    } else {
+      userInput.success('Right, click Next!😎');
+
+      btnRepeat.text = caption.NEXT;
+      repeatBtnMode = 'next';
+      btnRepeat.disabled = false;
+    }
+    keyboard.disabled = true;
+    return;
+  }
+  // wrong
+  if (userInput.value !== sequence.slice(0, len)) {
+    if (!attemptsLeft) {
+      userInput.error('You lost!🥴');
+    } else {
+      userInput.error('Wrong, try again!😟');
+    }
+    keyboard.disabled = true;
+  }
+};
