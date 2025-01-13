d56e119eff6c021404e16dba326c90458ed91406
dusixx <113414935+dusixx@users.noreply.github.com>
2025-01-13 05:29:18 +0200

    feat: update styles

--- simon-says/js/components/index.js
+++ simon-says/js/components/index.js
@@ -2,4 +2,4 @@ export * from './button.js';
 export * from './difficulty.js';
 export * from './keyboard.js';
 export * from './rounds-counter.js';
-export * from './text-input.js';
+export * from './user-input.js';
